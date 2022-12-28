46f0e592a6b0031ce3d13f6c70c16c571329ffb7
Alexandre Calaça <alexcalaca@gmail.com>
2022-12-28 16:21:17 -0300

    feat: create and use messages controller

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,8 @@
+class MessagesController < Rulers::Controller
+  def a_message
+    "hope which will not let us down, " + 
+    "because the love of God " +
+    "has been poured into our hearts by " +
+    "the Holy Spirit which has been given to us."
+  end
+end
