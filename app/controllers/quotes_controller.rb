c70aaf6d98b7f908fd3535835e0e1a76404dbb18
Alexandre Calaça <alexcalaca@gmail.com>
2022-12-29 09:12:12 -0300

    feat: exercise1 - add rack environment information

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,8 @@
 class QuotesController < Rulers::Controller
   def a_quote
     "There is nothing either good or bad " +
-    "but thinking makes it so."
+    "but thinking makes it so." +   
+    "\n<pre>\n#{env}\n</pre>" +
+    "\n<pre>\n#{env["GATEWAY_INTERFACE"]}\n</pre>" +
   end
  end
