ae043c43d4edaa3d9e9d16d86bec19508329176f
Alexandre Calaça <alexcalaca@gmail.com>
2022-12-29 09:27:26 -0300

    feat: exercise 2 - raise an exception

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,6 +3,10 @@ class QuotesController < Rulers::Controller
     "There is nothing either good or bad " +
     "but thinking makes it so." +   
     "\n<pre>\n#{env}\n</pre>" +
-    "\n<pre>\n#{env["GATEWAY_INTERFACE"]}\n</pre>" +
+    "\n<pre>\n#{env["GATEWAY_INTERFACE"]}\n</pre>"
+  end
+
+  def exception
+    raise "It's a bad one"
   end
  end
