8cc1964f68c1e99521df0cf38a0ece3917425f5e
Alexandre Calaça <alexcalaca@gmail.com>
2022-12-29 23:46:08 -0300

    load files out of controllers  directory

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require "rulers"
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
 require "quotes_controller"
 require "messages_controller"
+require "home_controller"
 
 module BestQuotes
   class Application < Rulers::Application
