5708f60252bcac4ff16258e3816503f09853a34a
Alexandre Calaça <alexcalaca@gmail.com>
2022-12-29 23:51:01 -0300

    feat: let load  files magically out of the  controllers  directory
    The LOAD_PATH line lets you load files out of 'app/controllers' just by requiring their name

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,8 @@
 require "rulers"
+
+# The LOAD_PATH line lets you load files out of “app/controllers” just by requiring their name
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+
 require "quotes_controller"
 require "messages_controller"
 require "home_controller"
